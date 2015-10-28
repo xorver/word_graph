a633739f32cf0db95d30aa94ae37213c2746c720
Tomasz Lichon <xorver@gmail.com>
2015-10-28 11:26:37 +0100

    Drawing script new layout.

--- draw.py
+++ draw.py
@@ -1,11 +1,12 @@
 import os, sys
 import graphviz as gv
+import subprocess
 
 main_word_dir = sys.argv[1]
 main_word = os.path.basename(main_word_dir)
 
 graph = gv.Digraph(format='svg')
-graph.node(main_word)
+graph.node(main_word, center="true")
 
 for file_name in os.listdir(main_word_dir):
     secondary_word = file_name[:-4]
@@ -13,8 +14,10 @@ for file_name in os.listdir(main_word_dir):
     graph.edge(main_word, secondary_word)
     with open(os.path.join(main_word_dir, file_name), encoding='utf-8') as file:
         for line in file:
-            [count, tertiary_word] = line[:-1].split(',')
+            [count, tertiary_word] = (line[:-1] if line.endswith('\n') else line).split(',')
             graph.node(tertiary_word)
-            graph.edge(secondary_word, tertiary_word, label=count)
+            len = float(count)/900
+            graph.edge(secondary_word, tertiary_word, len=str(10*(1-len)))
 
 graph.render('graph')
+subprocess.check_output(['neato', '-Tpdf', 'graph', '-o', 'graph.pdf'])
